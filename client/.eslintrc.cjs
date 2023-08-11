230a005864f0a2f83f28fe8e1e45ecf3c7890462
shonenenenene <danya.sytaev@gmail.com>
2023-08-11 15:13:41 +0300

    add some components

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    // 'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
@@ -10,13 +10,14 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
     "react/jsx-uses-react": "error",   
-    // "react/jsx-uses-vars": "error" 
+    "react/jsx-uses-vars": "error",
+    // 'react/prop-types': 'off',
   },
 }
