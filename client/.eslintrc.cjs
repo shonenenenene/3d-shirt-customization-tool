986ca9d2a95c5a2feea6a6e02848347098322a9c
shonenenenene <danya.sytaev@gmail.com>
2023-08-12 14:54:31 +0300

    add shirt model

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -18,6 +18,7 @@ module.exports = {
     ],
     "react/jsx-uses-react": "error",   
     "react/jsx-uses-vars": "error",
-    // 'react/prop-types': 'off',
+    'react/prop-types': 'off',
+    "react/no-unknown-property": "off",
   },
 }
